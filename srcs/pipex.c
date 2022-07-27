ae4d370bcf363c763d86371b3331aaf977fbae7e
AlecLavallee <alec.lavallee89@gmail.com>
2022-07-27 20:02:36 +0200

    Added function to keep track of the variables.
    Signed-off-by: AlecLavallee <alec.lavallee89@gmail.com>

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: alelaval <alelaval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 11:53:24 by alelaval          #+#    #+#             */
-/*   Updated: 2022/07/25 18:07:23 by alelaval         ###   ########.fr       */
+/*   Updated: 2022/07/27 18:18:15 by alelaval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ t_pipex	*init_all(void)
 	pipex = (t_pipex *)malloc((sizeof(t_pipex)));
 	if (!pipex)
 		error(pipex);
+	pipex->input = NULL;
+	pipex->output = NULL;
 	pipex->envp = NULL;
 	pipex->paths = NULL;
 	pipex->ret = 1;
@@ -40,7 +42,6 @@ pid_t	init_pid(t_pipex *pipex)
 
 void	fill_data(t_pipex *pipex)
 {
-	pipex->nb_cmds = 2;
 	// allocating memories for chain of commands
 	pipex->cmds = (t_comm**)malloc(sizeof(t_comm*) * 3 + 1);
 	pipex->cmds[0] = (t_comm*)malloc(sizeof(t_comm) * 1);
@@ -49,6 +50,7 @@ void	fill_data(t_pipex *pipex)
 	pipex->cmds[0]->args = (char **)malloc(sizeof(char *) * 3 + 1);
 	pipex->cmds[1]->args = (char **)malloc(sizeof(char *) * 2 + 1);
 	pipex->cmds[2]->args = (char **)malloc(sizeof(char *) * 2 + 1);
+	pipex->cmds[3] = NULL;
 	// Affecting commands and args
 	pipex->cmds[0]->args[0] = "cat";
 	pipex->cmds[0]->args[1] = "-e";
@@ -57,10 +59,35 @@ void	fill_data(t_pipex *pipex)
 	pipex->cmds[1]->args[0] = "wc";
 	pipex->cmds[1]->args[1] = "-l";
 	pipex->cmds[1]->args[2] = NULL;
-	pipex->cmds[2]->args[0] = "wc";
-	pipex->cmds[2]->args[1] = "-l";
+	pipex->cmds[2]->args[0] = "grep";
+	pipex->cmds[2]->args[1] = "1";
 	pipex->cmds[2]->args[2] = NULL;
-	pipex->cmds[3] = NULL;
+}
+
+void	debug_data(t_pipex *pipex)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (pipex->cmds[i])
+		i++;
+	pipex->nb_cmds = i;
+	printf("Number of commands : %d\n\n", pipex->nb_cmds);
+	printf("infile:%s\n", pipex->input);
+	printf("outfile:%s\n\n", pipex->output);
+	i = 0;
+	while (i < pipex->nb_cmds)
+	{
+		j = 0;
+		while (pipex->cmds[i]->args[j])
+		{
+			printf("cmd[%d]->arg[%d]:%s\n", i, j, pipex->cmds[i]->args[j]);
+			j++;
+		}
+		ft_putstr("\n");
+		i++;
+	}
 }
 
 void	ft_pipex(t_pipex *pipex)
@@ -72,27 +99,33 @@ void	ft_pipex(t_pipex *pipex)
 	executor(pipex);
 	waitpid(pid, &status, 0);
 	pipex->ret = WEXITSTATUS(status);
+	ft_putstr_fd("Work is done\n", 0);
+	exit (EXIT_SUCCESS);
 }
 
 int	main(int num_args, char **args, char **envp)
 {
-	t_pipex	*pipex;
-	char	**paths;
 
-	paths = NULL;
+	t_pipex	*pipex;
+	
 	pipex = NULL;
-	(void)num_args;
-	(void)args;
-	pipex = init_all();
-	paths = get_paths(pipex, envp);
-	pipex->envp = envp;
-	(void)paths;
-	// will be populated by Mariko's data later on
-	//parser(pipex, num_args, args, paths);
-	// write battery of tests
-	fill_data(pipex);
-	ft_pipex(pipex);
-	// no concerns for leaks right now
-	//free_all(pipex);
+	if (num_args)
+	{
+		(void)args;
+		pipex = init_all();
+		pipex->envp = get_paths(pipex, envp);
+		if (!pipex->envp)
+			printf("NULL ENVP!\n");
+		// will be populated by Mariko's data later on
+		//parser(pipex, num_args, args, paths);
+		// write battery of tests
+		fill_data(pipex);
+		debug_data(pipex);
+		printf("Running %d commands...\n", pipex->nb_cmds);
+		ft_pipex(pipex);
+		// no concerns for leaks right now
+		// write garbage collector (later?)
+		//free_all(pipex);
+	}
 	return (0);
 }
