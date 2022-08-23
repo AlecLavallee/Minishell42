88b605a2615976e1a7f5889580bbe7d682360cd9
Mariko TSUJI <mtsuji@e1r8p3.clusters.42paris.fr>
2022-08-23 13:04:07 +0200

    export is almost done (expected +=)

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -26,10 +26,48 @@ void    display_env_for_export(t_shell *shell)
     }
 }
 
+int check_argument_for_export(char *str)
+{
+    int i;
+
+    i = 0;
+    if (str[0] == '=' || (str[0] == '+' && str[1] == '='))
+        return (1);
+    while (str[i] && str[i] != '=' 
+        && (str[i] != '+' || str[i + 1] != '='))
+    {
+        if (i == 0 && ft_isdigit(str[i]) == 1)
+            return (1);
+        if (ft_isalnum(str[i]) == 0 && str[i] != '_')
+            return (1);
+        i++;    
+    }
+    return (0);
+}
+
 int    export(t_word *word, t_shell *shell)
 {
+    int res;
+
+    res = 0;
     if (word->next == NULL)
+    {
         display_env_for_export(shell);
+        return (0);
+    }
+    word = word->next;
+    while (word)
+    {
+        res = check_argument_for_export(word->str);
+        if (res != 0)
+        {
+            ft_printf("minishell : export: `%s' not a valid identifier\n", word->str); 
+            return (1);
+        }
+        else
+            env_add(word->str, shell);
+        word =word->next;
+    }
     return (0);
 }
 
